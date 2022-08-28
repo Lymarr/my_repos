ac689e1b03a12d368cabdd0fde9f410016679ae2
Olena <lena.lumar2003@gmail.com>
2022-08-28 18:06:25 +0200

    second commit

--- OOP/lab_1_oop/lab_1_oop/Program.cs
+++ OOP/lab_1_oop/lab_1_oop/Program.cs
@@ -7,6 +7,7 @@ namespace lab_1_oop
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Olena Lymar");
         }
     }
 }
